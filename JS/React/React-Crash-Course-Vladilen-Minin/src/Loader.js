337d44ba02333e0fc26f9b6644e1d7901c2e16e4
morozov1982 <morozov1982@gmail.com>
2020-01-15 11:00:57 +0600

    React ToDo-приложение с примером Прелодера и Модального окна

---
+++ JS/React/React-Crash-Course-Vladilen-Minin/src/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => (
+  <div style={{ display: 'flex', justifyContent: 'center', margin: '.5rem' }}>
+    <div className='lds-dual-ring' />
+  </div>
+)
+
+/********************************/
+/*** Preloader from *************/
+/*** https://loading.io/css *****/
+/********************************/
+/*** Используется в App.js ******/
+/********************************/

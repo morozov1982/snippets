91e76f491d8f53b2eeaf4b84476dc85e64f905ef
morozov1982@gmail.com <morozov1982@gmail.com>
2020-09-01 09:13:33 +0600

    Продолжаем заниматься Python и Django

--- ___test/001-django/mysite/mysite/urls.py
+++ ___test/001-django/mysite/mysite/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# from news.views import *  # index, test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('news/', index),
+    # path('test/', test)
+    path('', include('news.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
